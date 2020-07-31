0b0f4b8cb71b8f7f7210ad4717171aa71ec81bda
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-31 14:50:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(str_sent)
-  str_sent.split.collect do |sentence|
-    (sentence.reverse).join(" ")
+  str_sent.split.collect { |sentence| sentence.reverse}.join(" ")
   end
 end
